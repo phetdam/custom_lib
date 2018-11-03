3d42b5012e659204dfc0f8c15d30c52fe473a0a3
Derek <djh458@stern.nyu.edu>
2018-11-03 00:56:08 -0400

    renamed main.c; now testing d_array package

--- custom_lib_test.c
+++ custom_lib_test.c
@@ -1,3 +1,21 @@
+/**
+ * custom_lib_test.c
+ *
+ * quick program to test whatever features are being written for custom_lib.
+ *
+ * Changelog:
+ *
+ * 11-03-2018
+ *
+ * name of file changed to custom_lib_test.c, with additional macros for help string,
+ * program name, curretn package being tested, help flag and string added. main body 
+ * rewritten to test d_array package and to incorporate macros.
+ *
+ * ??-??-2018
+ *
+ * not entirely sure when this was created; originally titled main.c
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -6,6 +24,15 @@
 //#include "stats.h"
 #include "d_array.h"
 // program name
+#define PROGNAME "custom_lib_test"
+// help flag
+#define HELP_FLAG "--help"
+// current package being tested (update as necessary)
+#define CUR_TEST "d_array"
+// help string
+#define HELP_STR "Usage: " PROGNAME " [ " HELP_FLAG " ]\n"\
+    "drive program to test any features in custom_lib.\n\n"\
+    "current package being tested: " CUR_TEST
 
 // driver program to test any features in the custom_lib
 int main(int argc, char **argv) {
@@ -13,42 +40,23 @@ int main(int argc, char **argv) {
     // run normally if there are no arguments
     /*** TESTING AREA ***/
     if (argc == 1) {
-	// why is the distribution for my old normalcdf using the erf not symmetric? weird
-	// test using a bunch of values; normalcdf up to mean and then 1- normalcdf afterwards
-	// compare values and see what's up
 
-	// x, start and step
-	double x, m, step;
-	m = -3;
-	step = 0.1;
-	x = m - step;
-	// standardized mu and s
-	double mu = 0;
-	double s = 1;
-	// print headers
-	printf("        %15s         %15s\n", "normalcdf()", "1-normalcdf()");
-	// while x is less than or equal to mean
-	while ((x += step) <= mu + step) {
-	    printf("%+7.3lf %15.9lf %+7.3lf %15.9lf\n", x, normalcdf(x, mu, s), -1 * x, 1 - \
-		   normalcdf(-1 * x, mu, s));
-	}
     }
     // else if there is one argument
     else if (argc == 2) {
 	// if it is the help string
-	if (strcmp(argv[1], "--help") == 0) {
-	    printf("usage: %s [ --help ]\n"
-		   "driver program to test any features in the custom_lib\n", argv[0]);
+	if (strcmp(argv[1], HELP_FLAG) == 0) {
+	    printf("%s\n", HELP_STR);
 	    return 0;
 	}
 	// else unrecognized; print error and exit
-	fprintf(stderr, "%s: unknown flag \'%s\'. please type \'%s --help\' for usage.\n", argv[0], \
-		argv[1], argv[0]);
+	fprintf(stderr, "%s: unknown flag \'%s\'. please type \'%s %s\' for usage.\n",
+		PROGNAME, argv[1], PROGNAME, HELP_FLAG);
 	return 0;
     }
     // else too many arguments; print error and exit
     else {
-	printf("%s: too many arguments. type \'%s --help\' for usage.\n", argv[0], argv[0]);
+	printf("%s: too many arguments. type \'%s %s\' for usage.\n", PROGNAME, PROGNAME, HELP_FLAG);
     }
     return 0;
 }
