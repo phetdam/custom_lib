9157968373479fcbfde25f850cd72b51e2a5d19d
Derek <djh458@stern.nyu.edu>
2018-11-16 22:24:00 -0500

    updated comments for insert and append functions, and added function declaration and macro for d_array__set

--- d_array.h
+++ d_array.h
@@ -11,6 +11,11 @@
  *
  * Changelog:
  *
+ * 11-16-2018
+ *
+ * updated comments so that they reflect the comments in d_array.c, and added the
+ * function declaration for d_array__set. added function name macro for set.
+ *
  * 11-10-2018
  *
  * added macros and function declarations for d_array__get and d_array__getcpy. 
@@ -45,6 +50,7 @@
 #define D_ARRAY__REMOVE_N "d_array__remove"
 #define D_ARRAY__GET_N "d_array__get"
 #define D_ARRAY__GETCPY_N "d_array__getcpy"
+#define D_ARRAY__SET_N "d_array__set"
 // struct for dynamic array
 struct d_array {
     // point to an element (to serve as an array)
@@ -59,11 +65,11 @@ typedef struct d_array d_array;
 // n is the no. elements tha can be added before a resize is needed, e is the size of each
 // element in the array (in bytes). n and e must be positive.
 d_array *d_array__new(size_t n, size_t e);
-// inserts an item e of size da->e_siz into the d_array struct at index i. one cannot
-// insert to an index less than 0 or greater than da->siz - 1. cannot insert NULL.
-// please do not try and mix types, for your own sanity. 
+// writes da->e_siz bytes from e into the d_array struct at index i, effectively inserting
+// a new element into da. one cannot insert to an index less than 0 or greater than
+// da->siz - 1, or insert NULL. please do not try and mix types, for your own sanity.
 void d_array__insert(d_array *da, void *e, size_t i);
-// for d_array da, appends an item size da->e_siz to da->a at index da->siz, and then
+// for d_array da, appends an item to da->a at index da->siz, writing da->e_siz bytes from e.
 // increments da->siz. trying to mix types is not recommended.
 void d_array__append(d_array *da, void *e);
 // for d_array da, removes an item at index i, where i >= 0 and i < da->siz. da->siz will be
@@ -76,6 +82,9 @@ void *d_array__get(d_array *da, size_t i);
 // for an element located at address p, for d_array da, the ith element of da will be
 // written directly to the address at p.
 void d_array__getcpy(void *p, d_array *da, size_t i);
+// for an element located at address p, for the d_array da, da->e_siz bytes from p will
+// overwrite the ith element in da.
+void d_array__set(d_array *da, size_t i, void *p);
 // frees a d_array struct
 void d_array__free(d_array *da);
 
